de6df5d8ba4cd3023e684e74a02cbe2c2d8b4542
Maksim Bahmackij <maxim.bahmackij@gmail.com>
2024-03-22 00:55:45 +0200

    feat: refactor SubmitButton component

--- app/components/common/ui/SubmitButton/index.tsx
+++ app/components/common/ui/SubmitButton/index.tsx
@@ -2,13 +2,31 @@ import { FC } from "react";
 
 export interface SubmitButtonProps {
   title: string;
+  tone?: string;
+  toneHover?: string;
+  size?: "small" | "medium" | "large";
 }
 
-const SubmitButton: FC<SubmitButtonProps> = ({ title }) => {
+const SubmitButton: FC<SubmitButtonProps> = ({
+  title,
+  size = "medium",
+  tone = "bg-slate-600",
+  toneHover = "bg-blue-500",
+}) => {
+  let sizeCss = "px-4 py-2";
+
+  if (size === "small") {
+    sizeCss = "px-2 py-1";
+  }
+
+  if (size === "large") {
+    sizeCss = "px-5 py-3";
+  }
+
   return (
     <button
       type="submit"
-      className="rounded bg-slate-600 px-4 py-2 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+      className={`rounded ${sizeCss} ${tone} text-blue-100 hover:${toneHover} active:bg-blue-600`}
     >
       {title}
     </button>
